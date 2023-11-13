04bd56435f16cb99685f1fa17ad096a7bb62a2a9
liam buchanan <=>
2023-11-13 15:45:25 +0000

    printing question

--- Coursework.java
+++ Coursework.java
@@ -1,10 +1,13 @@
 public class CardGame {
     public static void main(String[] args) {
+        System.out.println("How many cards");
     }
 }
 
+
 class Card {
-    //card class    
+    //card class   
+
 }
 class Player {
     //player class    
