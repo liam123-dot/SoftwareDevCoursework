b59cdcee21b8f6403feb72043eff56e5d9d7d8cc
af676 <af676@exeter.ac.uk>
2023-11-19 21:13:15 +0000

    creating classes

--- Card.java
+++ Card.java
@@ -12,10 +12,4 @@ class Card {
     }
 
 }
-class Player {
 
-    // each player has a private hand of cards
-    private Card[] hand = new Card[4];
-
-    //player class    
-}
--- CardGame.java
+++ CardGame.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.io.File;
-import Coursework.Card;
+import Card;
 
 public class CardGame {
 
