5ccab34690e04911a5ab525b8230e0a6729ec9c9
af676 <af676@exeter.ac.uk>
2023-11-29 11:16:56 +0000

    remove dup test

--- TestPlayer.java
+++ TestPlayer.java
@@ -70,6 +70,7 @@ public class TestPlayer {
     }
     @Test
     public void testAddCardToHand() {
+        //also tests getHand() 
         CardDeck previousDeck = new CardDeck(1);
         CardDeck nextDeck = new CardDeck(2);
         GameState gameState = new GameState(new CardDeck[0]);
@@ -88,26 +89,6 @@ public class TestPlayer {
         assertArrayEquals(new Card[]{card1, card2, card3, card4}, player.getHand());
     }
 
-    @Test
-    public void testGetHand() {
-        CardDeck previousDeck = new CardDeck(1);
-        CardDeck nextDeck = new CardDeck(2);
-        GameState gameState = new GameState(new CardDeck[0]);
-        Player player = new Player(1, previousDeck, nextDeck, gameState);
-
-        Card card1 = new Card(1);
-        Card card2 = new Card(2);
-        Card card3 = new Card(3);
-        Card card4 = new Card(4);
-
-        player.addCardToHand(card1);
-        player.addCardToHand(card2);
-        player.addCardToHand(card3);
-        player.addCardToHand(card4);
-
-        assertArrayEquals(new Card[]{card1, card2, card3, card4}, player.getHand());
-
-    }
 
     @Test
     public void testToString() {
